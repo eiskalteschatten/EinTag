d899a69f1a08a1eb2908d3675623b4ce6a435a98
Alex <alex@alexseifert.com>
2020-12-19 10:44:07 +0100

    Initial Commit

---
+++ Shared/EinTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  EinTagApp.swift
+//  Shared
+//
+//  Created by Alex Seifert on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct EinTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
