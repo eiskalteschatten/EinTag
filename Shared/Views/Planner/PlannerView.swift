c4399550f1e6a994e363e2727bbf5e14b09d78c4
Alex <alex@alexseifert.com>
2020-12-20 23:20:16 +0100

    Fix the planner view

--- Shared/Views/Planner/PlannerView.swift
+++ Shared/Views/Planner/PlannerView.swift
@@ -8,20 +8,22 @@
 import SwiftUI
 
 struct PlannerView: View {
+    @ObservedObject var plannerData = PlannerData()
+    
     var body: some View {
         ScrollView {
             VStack {
-//                let calendar = Calendar.current
-//                let today = calendar.startOfDay(for: Date())
-//                let dayOfWeek = calendar.component(.weekday, from: today)
-//                let weekdays = calendar.range(of: .weekday, in: .weekOfYear, for: today)!
-//                let days = (weekdays.lowerBound ..< weekdays.upperBound)
-//                    .compactMap { calendar.date(byAdding: .day, value: $0 - dayOfWeek, to: today) }
-//                
-//                ForEach(days, id: \.self) { days in
-//                    PlannerDayView(date: days)
-//                        .padding(.horizontal)
-//                }
+                let calendar = Calendar.current
+                let today = calendar.startOfDay(for: Date())
+                let dayOfWeek = calendar.component(.weekday, from: today)
+                let weekdays = calendar.range(of: .weekday, in: .weekOfYear, for: today)!
+                let days = (weekdays.lowerBound ..< weekdays.upperBound)
+                    .compactMap { calendar.date(byAdding: .day, value: $0 - dayOfWeek, to: today) }
+                
+                ForEach(days, id: \.self) { days in
+                    PlannerDayView(date: days, plannerData: plannerData)
+                        .padding(.horizontal)
+                }
             }
         }
         .toolbar {
